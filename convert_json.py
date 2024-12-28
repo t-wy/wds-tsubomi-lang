ea3ec72c307e0b0de27a41f44ff311219bc88915
t-wy <24794414+t-wy@users.noreply.github.com>
2024-12-28 20:30:31 +0800

    Upload Samples

---
+++ convert_json.py
@@ -0,0 +1,7 @@
+import csv, json
+from os.path import dirname, abspath
+path = dirname(abspath(__file__))
+with open(path + "/data.csv") as csvfile:
+    with open(path + "/data.json", "w") as jsonfile:
+        reader = csv.DictReader(csvfile, delimiter=",", quotechar='"')
+        json.dump(list(reader), jsonfile, ensure_ascii=False, indent=4)
