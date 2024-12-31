ff1c90b41dc3705f228c0b3cc075bba35022246f
t-wy <24794414+t-wy@users.noreply.github.com>
2024-12-31 20:24:23 +0800

    Add js version for webpages

--- scripts/csv_to_json.py
+++ scripts/csv_to_json.py
@@ -1,10 +1,17 @@
 import csv, json
+from io import StringIO
 from os.path import dirname, abspath
 path = dirname(abspath(__file__))
 with open(path + "/../data.csv") as csvfile:
-    with open(path + "/../data.json", "w") as jsonfile:
+    with StringIO() as jsonfile:
         reader = csv.DictReader(csvfile, delimiter=",", quotechar='"')
         entries = list(reader)
         for entry in entries:
             entry["hasruby"] = entry.get("hasruby", "0") == "1"
         json.dump(entries, jsonfile, ensure_ascii=False, indent=4)
+        jsonfile.seek(0)
+        json_content_from_csv = jsonfile.read()
+    with open(path + "/../data.json", "w") as jsonfile:
+        jsonfile.write(json_content_from_csv)
+    with open(path + "/../data.js", "w") as jsonfile:
+        jsonfile.write(f"var tsubomi_data = {json_content_from_csv};")
--- scripts/json_to_csv.py
+++ scripts/json_to_csv.py
@@ -2,10 +2,13 @@ import csv, json
 from os.path import dirname, abspath
 path = dirname(abspath(__file__))
 with open(path + "/../data.json") as jsonfile:
-    entries = json.load(jsonfile)
+    json_content = jsonfile.read()
+    entries = json.loads(json_content)
     for entry in entries:
         entry["hasruby"] = int(entry.get("hasruby", False))
     with open(path + "/../data.csv", "w", newline="") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=entries[0].keys(), delimiter=",", quotechar='"', lineterminator="\n")
         writer.writeheader()
         writer.writerows(entries)
+    with open(path + "/../data.js", "w") as jsonfile:
+        jsonfile.write(f"var tsubomi_data = {json_content};")
